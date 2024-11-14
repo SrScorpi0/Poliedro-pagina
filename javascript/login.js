0f7166a4c432863178936f4fb1a766f41e3e6f47
Axel Rion <axelrion168@gmail.com>
2024-11-13 23:59:26 -0300

    Se creo la seccion Login y el boton de inicio de sesion en la landing page (queda terminar de darle el estilo correcto

---
+++ javascript/login.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("index.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error al cargar la página");
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById("main-content").innerHTML = data;
+        })
+        .catch(error => console.error("Error al cargar la página:", error));
+});
+/*
+function openLogin() {
+    document.getElementById('overlay').style.display = 'flex';
+}
+
+function closeLogin() {
+    document.getElementById('overlay').style.display = 'none';
+}
+*/
+function submitLogin() {
+    alert('Iniciando sesión...');
+    closeLogin();
+}
